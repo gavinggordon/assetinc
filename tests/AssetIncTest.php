9ebe118d4a1c096b25c0bd99417960533211eee1
Gavin G Gordon <gmarkowski@my.bcit.ca>
2016-04-08 07:41:44 -0700

    Updated AssetIncTest.php
    Updated AssetIncTest.php to reflect recent change to AssetInc.php

--- tests/AssetIncTest.php
+++ tests/AssetIncTest.php
@@ -7,21 +7,21 @@ class AssetIncTest extends \PHPUnit_Framework_TestCase
 
 	public function testInsertJs()
     {
-		$JSasset = new AssetInc( 'js' );
+		$JSasset = AssetInc::init( 'js' );
 		$expected = '<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>';
         $this->assertSame( $expected, $JSasset->insert( 'jquery' ) );
     }
 	
 	public function testInsertCss()
     {
-		$CSSasset = new AssetInc( 'css' );
+		$CSSasset = AssetInc::init( 'css' );
 		$expected = '<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" rel="stylesheet">';
         $this->assertSame( $expected, $CSSasset->insert( 'font-awesome' ) );
     }
 	
 	public function testAdd_AssetJs()
     {
-		$JSasset = new AssetInc( 'js' );
+		$JSasset = AssetInc::init( 'js' );
 		$JSasset->add_asset( 'test-js-asset', 'https://www.test-js-asset.com/test.js' );
 		$expected = 'https://www.test-js-asset.com/test.js';
         $this->assertSame( $expected, $JSasset->insert( 'test-js-asset' ) );
@@ -29,7 +29,7 @@ class AssetIncTest extends \PHPUnit_Framework_TestCase
 	
 	public function testAdd_AssetCss()
     {
-		$CSSasset = new AssetInc( 'css' );
+		$CSSasset = AssetInc::init( 'css' );
 		$CSSasset->add_asset( 'test-css-asset', 'https://www.test-css-asset.com/test.css' );
 		$expected = 'https://www.test-css-asset.com/test.css';
         $this->assertSame( $expected, $CSSasset->insert( 'test-css-asset' ) );
